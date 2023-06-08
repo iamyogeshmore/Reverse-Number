5298014c6b7c15e3c76604e0e1bf9f539f02a95a
Yogesh More <yogesh.more2098@gmail.com>
2023-06-08 22:58:57 +0530

    Added: Reverse a number using recursion.

---
+++ src/main/java/org/bridgelabz/ReverseNumber.java
@@ -0,0 +1,23 @@
+package org.bridgelabz;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void reverseNumber(int number) {
+        if (number < 10) {
+            System.out.println(number);  //prints the same number if the number is less than 10
+            return;
+        } else {
+            System.out.print(number % 10);
+            reverseNumber(number / 10);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter the number that you want to reverse: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.print("The reverse of the given number is: ");
+        reverseNumber(num);
+    }
+}
